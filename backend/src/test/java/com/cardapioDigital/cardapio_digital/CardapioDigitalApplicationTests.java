5a8aac952f84e2f91bf44bf57170baac11b91d70
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-09-23 13:54:00 -0300

    projeto springboot criado na pasta backend

---
+++ backend/src/test/java/com/cardapioDigital/cardapio_digital/CardapioDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardapioDigital.cardapio_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
