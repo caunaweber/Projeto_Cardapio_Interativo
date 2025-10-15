a6ab9ddc266005b989e08edd7c7a208c379c0bc8
cWeber <cauna_weber@estudante.sesisenai.org.br>
2025-10-15 11:49:07 -0300

    alterando nome de pedidoController para Cardapio controller

--- backend/src/main/java/com/cardapioDigital/cardapio_digital/controller/CardapioController.java
+++ backend/src/main/java/com/cardapioDigital/cardapio_digital/controller/CardapioController.java
@@ -2,26 +2,35 @@ package com.cardapioDigital.cardapio_digital.controller;
 
 import com.cardapioDigital.cardapio_digital.dto.CreateComandaDto;
 import com.cardapioDigital.cardapio_digital.dto.ResponseComandaDto;
+import com.cardapioDigital.cardapio_digital.dto.ResponsePratoDto;
 import com.cardapioDigital.cardapio_digital.model.Comanda;
 import com.cardapioDigital.cardapio_digital.service.ComandaService;
+import com.cardapioDigital.cardapio_digital.service.PratoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/pedidos")
-public class PedidoController {
+@RequestMapping("/cardapio")
+public class CardapioController {
 
     @Autowired
     private ComandaService comandaService;
+
+    @Autowired
+    private PratoService pratoService;
 
     @PostMapping
     public ResponseEntity<ResponseComandaDto> createComanda(@RequestBody @Valid CreateComandaDto dto){
         Comanda comanda = comandaService.createComanda(dto);
         return ResponseEntity.ok().body(ResponseComandaDto.createComandaResponse(comanda));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponsePratoDto>> getPratos() {
+        return ResponseEntity.ok().body(pratoService.getAllPratos());
     }
 }
